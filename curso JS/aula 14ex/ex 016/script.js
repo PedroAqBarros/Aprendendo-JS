1f4c02bf8183a07d482822d178710a66d4d6d0bd
Pedro Aquino Barros <pkzinfps@gmail.com>
2025-02-24 16:09:10 -0300

    dia 07

--- curso JS/aula 14ex/ex 016/script.js
+++ curso JS/aula 14ex/ex 016/script.js
@@ -1,12 +1,29 @@
 function contar() {
-    let inicio = document.getElementById('txti')
+    let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-    
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'impossivel contar'
     } else {
-        res.innerHtml = 'Contando: '
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo invalido!, considerando passo 1')
+            p = 1
+        }
+        if (i < f) { //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} `
+            }
+        }else{//contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} `
+            }
+        }
     }
 }
